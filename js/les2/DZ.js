d97ca6cdab412ef4f5c70332bfbbbbc6d572a077
jaocbs <jmparnes@gmail.com>
2016-12-12 23:53:59 +0300

    домашнее задание к 10.12.2016

--- js/les2/DZ.js
+++ js/les2/DZ.js
@@ -41,8 +41,8 @@ if (Math.round(xy2) == Math.round(xz2) + Math.round(yz2)) {
 var arr = [100, 4, 10, 3, 16, 8, 549, 234, 22];
 var emptyArr = [];
 
-var n = arr.length;
 console.log(n);
+var n = arr.length;
 console.log(arr);
 for (var i = 0; i < n-1; i++) {
 
@@ -59,7 +59,7 @@ for (var i = 0; i < n-1; i++) {
 // 3. Написать программу для перевода числа из десятичной системы счисления в двоичную и наоборот. Вам понадобится метод 
 // Math.floor(), чтобы отбросить дробную часть и метод pow() объекта Math, он возводит число в степень
 
-var num10 = 32;
+var num10 = 54;
 var num2 = [];
 
 
@@ -69,7 +69,7 @@ console.log('dec number', num10);
     }
 while (num10 >= 1) {
     if (num10 == 0) {
-        num2.puss(0);
+        num2.push(0);
     } else if (num10 % 2 == 0) {
         num10 = num10 / 2;
         num2.push(0);
@@ -78,8 +78,22 @@ while (num10 >= 1) {
             num2.push(1);
         }
 }
-
 console.log('binary number', num2.reverse());
+var num22 = num2.reverse();
+
+var t = [];
+var numDec = 0;
+for (i = 0; i < num2.length; i++) {
+    if (num22[i] == 1) {  
+        t[i] = Math.pow(2, i);
+    } else t[i] = 0;
+    numDec = numDec +t[i]; 
+}
+console.log('десятичное число = ', numDec);
+
+
+
+
 
 
 
